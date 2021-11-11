b1ee5b8e0a882975d61670eef383c6af8c6f509c
avidaldo <avd.formacion@gmail.com>
2021-11-11 18:52:10 +0100

    Tirador de dados básico

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.diceroller.databinding.ActivityMainBinding // Importamos la clase generada de vinculación
+import kotlin.random.Random
+
+// https://classroom.udacity.com/courses/ud9012
+// Actualizando con ConstraintLayout y View Binding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // View Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun rollDice(view: View) {
+
+        /* Generación de números aleatorios (varias opciones equivalentes): */
+        val randomInt  = Random.nextInt(6) + 1
+        val randomInt2  = Random.nextInt(1,7) // Yo usaría ésta
+        val randomInt3  = Random.nextInt(until = 7, from = 1)
+        //val randomInt4  = Random.nextInt(7, 1)
+
+        /* Seteo de la caja de texto con el número generado (para pruebas) */
+        binding.textView.setText("$randomInt // $randomInt2 // $randomInt3")
+
+        /* Seteo de la imagen correspondiente al número generado */
+        val id_imagen = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> throw Exception()   // TODO: revisar excepciones
+        }
+        binding.imageView.setImageResource(id_imagen)
+
+
+    }
+
+    // https://classroom.udacity.com/courses/ud9012/lessons/37a8fa57-7d18-4704-bfb7-da2864cb2e75/concepts/3e9c9714-eb89-438e-8a06-2a75dd9f3719
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# android-tirador-dados
+Basado en el curso de Udacity: https://classroom.udacity.com/courses/ud9012
+pero utilizando ConstraintLayout y View Binding
+
